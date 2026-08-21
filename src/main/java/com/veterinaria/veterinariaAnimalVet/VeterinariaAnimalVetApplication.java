225c9fcc75a0bde1f5587506cc6d6c1d98efb727
Dulce Navarro <navarro.dulce2006@gmail.com>
2026-08-20 21:57:38 -0500

    commit inicial

---
+++ src/main/java/com/veterinaria/veterinariaAnimalVet/VeterinariaAnimalVetApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria.veterinariaAnimalVet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaAnimalVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaAnimalVetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veterinaria/veterinariaAnimalVet/VeterinariaAnimalVetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.veterinariaAnimalVet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaAnimalVetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
