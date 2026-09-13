bd329c76b65658ade9cdeccfef0dbdcff4f48afb
Dulce Navarro <navarro.dulce2006@gmail.com>
2026-09-13 00:39:23 -0500

    relaciones cliente

--- src/main/java/com/veterinaria/veterinariaAnimalVet/model/Cliente.java
+++ src/main/java/com/veterinaria/veterinariaAnimalVet/model/Cliente.java
@@ -26,4 +26,61 @@ public class Cliente {
     @Column(nullable = false, length = 9)
     private String telefono;
 
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Mascota> mascotas;
+
+    // Relación OneToMany: Un cliente puede tener muchas citas (a través de sus mascotas)
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Cita> citas;
+    
+    public Cliente() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public List<Mascota> getMascotas() {
+        return mascotas;
+    }
+
+    public void setMascotas(List<Mascota> mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    public List<Cita> getCitas() {
+        return citas;
+    }
+
+    public void setCitas(List<Cita> citas) {
+        this.citas = citas;
+    }
 }
